23d70e95993a5a83f78b1a65aa0e4b70f3c89ab0
Mohid <mohidrehman321@gmail.com>
2025-01-25 17:57:42 +0000

    remove this random error ting

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
-import Navbar from './navbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
