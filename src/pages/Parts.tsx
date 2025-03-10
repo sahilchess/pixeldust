f3e5b7a1014bbf78f79ef60e0caf31c37f1a0000
Mohid <mohidrehman321@gmail.com>
2025-03-10 23:00:13 +0000

    accidentally pushed broken file 	modified:   src/pages/Parts.tsx

--- src/pages/Parts.tsx
+++ src/pages/Parts.tsx
@@ -1,19 +1,10 @@
-const Parts = () => {
+
+const Guides = () => {
     return (
-        <div className="flex flex-col items-center justify-center w-full h-[calc(100vh-4rem)] text-2xl text-black">
-            <h1>Allowed Parts</h1>
-            <h2>MCU:</h2>
-            <ul>
-                <li>Xiao RP2040 (will be soldered by you!)</li>
-                <li>Custom RP2040 for ppl who know a lot about pcb design</li>
-            </ul>
-            <h2>LEDS:</h2>
-            <ul>
-                <li>WS2812B</li>
-                <li>SK6812</li>
-            </ul>
+        <div className="flex items-center justify-center w-full h-[calc(100vh-4rem)] text-2xl text-white">
+            Coming soon!
         </div>
     );
 };
 
-export default Parts; 
+export default Guides; 
