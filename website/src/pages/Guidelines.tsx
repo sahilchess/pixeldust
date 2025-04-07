c96e8c85c6d8183fe2783c3c2882575dc1378f9a
Toby Cm <toby@vsus.app>
2025-04-07 12:14:58 -0700

    improve submission guidelines

--- website/src/pages/Guidelines.tsx
+++ website/src/pages/Guidelines.tsx
@@ -237,7 +237,7 @@ const Guidelines = () => {
 
         <p className="mt-4">
           In the submission folder, create a new folder with your board name. Copy README.md from the <code>!Template</code> folder and fill in your
-          info.
+          info. Please add a lots of pictures of your board!! Required photos: top/bottom side 3D render
         </p>
 
         <p className="mt-4">Generate a PDF of your schematic and a BOM and add it in your submission folder!</p>
